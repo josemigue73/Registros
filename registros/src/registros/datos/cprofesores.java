15ac29e56535dfe7610a73a7d2c01e84b84f0871
sangrosferatus <maxi.mus.2007@hotmail.com>
2019-11-04 14:54:32 -0500

    creacion de la clase profesores

---
+++ registros/src/registros/datos/cprofesores.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registros.datos;
+
+/**
+ *
+ * @author Sangrosferatus
+ */
+public class cprofesores {
+    private int Cod_Profesor;
+    private String Docente;
+    private int Num_Documento;
+
+    public cprofesores() {
+    }
+
+    public cprofesores(int Cod_Profesor, String Docente, int Num_Documento) {
+        this.Cod_Profesor = Cod_Profesor;
+        this.Docente = Docente;
+        this.Num_Documento = Num_Documento;
+    }
+
+    public int getCod_Profesor() {
+        return Cod_Profesor;
+    }
+
+    public void setCod_Profesor(int Cod_Profesor) {
+        this.Cod_Profesor = Cod_Profesor;
+    }
+
+    public String getDocente() {
+        return Docente;
+    }
+
+    public void setDocente(String Docente) {
+        this.Docente = Docente;
+    }
+
+    public int getNum_Documento() {
+        return Num_Documento;
+    }
+
+    public void setNum_Documento(int Num_Documento) {
+        this.Num_Documento = Num_Documento;
+    }
+
+    
+      
+}
+
+  
