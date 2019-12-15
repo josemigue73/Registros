e7b8584ae2ca3d035ed3fb683b49a852974626d8
sangrosfera <57327769+sangrosfera@users.noreply.github.com>
2019-12-15 16:25:38 -0500

    Update fmatricula.java

--- registros/src/logica/fmatricula.java
+++ registros/src/logica/fmatricula.java
@@ -25,7 +25,7 @@ public class fmatricula {
    private Connection cn=mysql.conectar();
    private String sSQL="";
    public Integer totalregistros; 
-   private String travis2;
+   private String travis4;
    
    
    public DefaultTableModel mostrar(String buscar){
