124c735dc380e3656e0bcd18764903870f8a12b4
sangrosferatus <maxi.mus.2007@hotmail.com>
2019-12-15 15:40:57 -0500

    prueba 2

--- registros/src/logica/fmatricula.java
+++ registros/src/logica/fmatricula.java
@@ -25,7 +25,7 @@ public class fmatricula {
    private Connection cn=mysql.conectar();
    private String sSQL="";
    public Integer totalregistros; 
-   private String travis;
+   private String travis1;
    
    
    public DefaultTableModel mostrar(String buscar){
